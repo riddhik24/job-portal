4c12951edde8ad0dde6a60d558f3a577f1bfb29e
Riddhik <riddhikmohite@gmail.com>
2026-08-20 15:38:45 +0530

    endpoint changed

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api/",
+  // baseURL: "http://localhost:5000/api/",
+  baseURL: import.meta.env.API_BASE_URL,
   withCredentials: true,
 });
